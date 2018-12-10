c7388d3971824900c0e87ed66439ab4710400777
Jose Then <thenj@ymail.com>
2018-12-09 23:37:56 -0500

    Update on options page

--- js/options.js
+++ js/options.js
@@ -1,16 +1,19 @@
-let page = document.getElementById('buttonDiv');
+const buttonDiv = document.getElementById('buttonDiv');
+
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-    for (item of kButtonColors) {
-        let button = document.createElement('button');
-        button.style.backgroundColor = item;
+function constructOptions(urlArray) {
+    urlArray.forEach(function (url)  {
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.style.backgroundColor = url;
         button.addEventListener('click', function () {
-            chrome.storage.sync.set({color: item}, function () {
-                console.log('color is ' + item);
-            })
+            chrome.storage.sync.set({color: url}, function () {
+                console.log(`color is  ${url}`);
+            });
         });
-        page.appendChild(button);
-    }
+        li.appendChild(button);
+        buttonDiv.appendChild(li);
+    });
 }
 
-constructOptions(kButtonColors)
+constructOptions(kButtonColors);
