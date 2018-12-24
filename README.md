5466e13ef3ab2fc41d22fe412345eee1a4ac2957
Jose Then <thenj@ymail.com>
2018-12-23 21:37:19 -0500

    Update

---
+++ README.md
@@ -0,0 +1,14 @@
+# Docs Hive
+
+## `A Chrome extension`
+
+This repository contains the code for `Docs Hive`, an extension that holds Popular Developer Documentation in a contained popup.
+
+## Functionalities
+
+    - clicking on a documentation button will open the doc site in a new page
+    - clicking a documentation button while a tab is on the doc site will move to that tab
+
+## Development
+
+Any one looking at this repository feel free to make any pull request or bring up any issues if you have **any** feature requests or you find some issues.
--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,35 @@
+function getAllTabUrls(callback) {
+    chrome.tabs.query({ url: "*://*/*"}, function(tabs) {
+        const tabUrls = {};
+        tabs.forEach(function (tab) {
+            tabUrls[tab.url] = tab.id;
+        });
+        console.log(tabUrls);
+        return callback(tabUrls);
+    });
+}
 
+function goTo(tabId) {
+    chrome.tabs.update(tabId, {active: true});
+    return false;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    getAllTabUrls(function (urls) {
+        const tabList = urls;
+        document.querySelectorAll('ul.docks > a').forEach(function (dock) {
+            const dockUrl = new URL(dock.attributes.href.value);
+            const found = Object.keys(tabList).find(function (tab) {
+                return tab.match(dockUrl.hostname);
+            });
+            if (found !== undefined) {
+                console.log(dock);
+                // dock.onclick = goTo(tabList[found]);
+                dock.addEventListener('click', function () {
+                    goTo(tabList[found]);
+                });
+                dock.childNodes[1].setAttribute('class', 'active dock');
+            }
+        });
+    });
+});
